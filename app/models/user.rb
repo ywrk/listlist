e35ed266a3a7ec815380c7ecd2da29e709c4d6e1
ywrk <ywrk.wprg@gmail.com>
2020-12-08 13:03:54 +0900

    deviseの導入

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  has_many :firstlists
+
+  with_options presence: true do
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)\w{6,}\z/.freeze
+    validates :nickname,length: { maximum: 6 }
+    validates :password, format: { with: VALID_PASSWORD_REGEX, message: '半角6文字以上で英字と数字の両方を含めて設定してください' }
+  end
+
+end
