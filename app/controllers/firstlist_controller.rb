b9bffaabd1ff0e7f3ee00f1cce0961aae6b36437
ywrk <ywrk.wprg@gmail.com>
2020-12-10 00:50:39 +0900

    投稿フォームとREADME

--- app/controllers/firstlist_controller.rb
+++ app/controllers/firstlist_controller.rb
@@ -2,4 +2,24 @@ class FirstlistController < ApplicationController
   def index
     @firstlists = Firstlist.all
   end
+
+  def new
+    @firstlist = Firstlist.new
+  end
+
+  def create
+    @firstlist = Firstlist.new(firstlist_params)
+    if @firstlist.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def firstlist_params
+    params.require(:firstlist).permit(:title, :text1, :text2, :text3, :text4, :text5, :text6).merge(user_id: current_user.id)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "firstlist#index"
 
-  resources :firstlist, only: [:index]
+  resources :firstlist, only: [:index, :new, :create]
   
 end
