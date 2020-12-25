7b13430c3bc0cad712eeff93d8a68f13091eed90
ywrk <ywrk.wprg@gmail.com>
2020-12-25 17:45:44 +0900

    add action for guest login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
     validates :password, format: { with: VALID_PASSWORD_REGEX, message: '半角6文字以上で英字と数字の両方を含めて設定してください' }
   end
 
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.nickname = "ゲストユーザー"
+      # user.confirmed_at = Time.now  # Confirmable を使用する場合は必要
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "firstlists#index"
+  devise_for :users
+
+  devise_scope :user do
+    post 'users/sign_in/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 
   resources :firstlists do
     member do
       get "auto_select"
     end
   end
+  
   
 end
