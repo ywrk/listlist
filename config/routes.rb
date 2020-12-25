c2248176197ffdbd030fcbedc8ac99bf9d2062f3
ywrk <ywrk.wprg@gmail.com>
2020-12-25 18:30:48 +0900

    can not delete guest user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "firstlists#index"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   devise_scope :user do
     post 'users/sign_in/guest_sign_in', to: 'users/sessions#new_guest'
