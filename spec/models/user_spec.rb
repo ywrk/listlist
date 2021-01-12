cb3673818998f58dacde86569663972444a54971
ywrk <ywrk.wprg@gmail.com>
2021-01-12 23:52:05 +0900

    Add factoryBot

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+    end
+    it "emailが空では登録できない" do
+    end
+  end
+end
