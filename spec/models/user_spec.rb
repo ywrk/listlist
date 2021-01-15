f981fba0e9a0b323e0abc5238684e03c6f7ef4ca
ywrk <ywrk.wprg@gmail.com>
2021-01-15 23:51:53 +0900

    Add rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
+
+#  before do
+#    @user = FactoryBot.build(:user)
+#  end
+
   describe "ユーザー新規登録" do
     it "nicknameが空だと登録できない" do
       user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
       user.valid?
     end
     it "emailが空では登録できない" do
+      user = User.new(nickname: "@user.nickname", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
     end
+
+    
   end
 end
