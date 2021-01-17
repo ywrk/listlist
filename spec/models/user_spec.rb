4661ddc89b3a06c6ae59eed3377c58161896d63a
ywrk <ywrk.wprg@gmail.com>
2021-01-17 22:04:01 +0900

    Fix test code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
 
-#  before do
-#    @user = FactoryBot.build(:user)
-#  end
+ before do
+   @user = FactoryBot.build(:user)
+ end
 
-  describe "ユーザー新規登録" do
-    it "nicknameが空だと登録できない" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-      user.valid?
+ describe "ユーザー新規登録" do
+  context '新規登録がうまくいくとき' do
+    it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+      expect(@user).to be_valid
     end
-    it "emailが空では登録できない" do
-      user = User.new(nickname: "@user.nickname", email: "", password: "00000000", password_confirmation: "00000000")
-      user.valid?
+    it "nicknameが8文字以下であれば登録できる" do
+      @user.nickname = "aaaaaaaa"
+      expect(@user).to be_valid 
     end
-
-    
+    it "passwordが6文字以上であれば登録できる" do
+      @user.password = "000000"
+      @user.password_confirmation = "000000"
+      expect(@user).to be_valid 
+    end
   end
 end
+
+end
+
+
+    # it "nicknameが空だと登録できない" do
+    #   user = User.new(nickname: "", email: "testtest@gmail.com", password: "00000000", password_confirmation: "00000000")
+    #   user.valid?
+    # end
+    # it "emailが空では登録できない" do
+    #   user = User.new(nickname: "@user.nickname", email: "", password: "00000000", password_confirmation: "00000000")
+    #   user.valid?
+    # end
+
