bde66a199244ab15f842d9a14a32dea6113e0f9b
ywrk <ywrk.wprg@gmail.com>
2021-01-19 16:19:39 +0900

    Fixed read me

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 8)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"aaa111"}
     password_confirmation {password}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,66 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
 
- before do
-   @user = FactoryBot.build(:user)
- end
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
- describe "ユーザー新規登録" do
-  context '新規登録がうまくいくとき' do
-    it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
-      expect(@user).to be_valid
+  describe "ユーザー新規登録" do
+    context '新規登録がうまくいくとき' do
+      it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "nicknameが8文字以下であれば登録できる" do
+        @user.nickname = "aaaaaaaa"
+        expect(@user).to be_valid 
+      end
+      it "passwordが6文字以上の英数字混合であれば登録できる" do
+        @user.password = "aaa000"
+        @user.password_confirmation = "aaa000"
+        expect(@user).to be_valid 
+      end
     end
-    it "nicknameが8文字以下であれば登録できる" do
-      @user.nickname = "aaaaaaaa"
-      expect(@user).to be_valid 
+    context '新規登録がうまくいかないとき' do
+      it "nicknameが空だと登録できない" do
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nicknameを入力してください")
+      end
+      it "emailが空では登録できない" do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Emailを入力してください")
+      end
+      it "passwordが空では登録できない" do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Passwordを入力してください")
+      end
+      it "password_confirmationが空では登録できない" do
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmationとPasswordの入力が一致しません")
+      end
+      it "password_confirmationがpasswordと一致しなければ登録できない" do
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmationとPasswordの入力が一致しません")
+      end
+      it "nicknameが8文字以上だと登録できない" do
+        @user.nickname = "aaaaaaaa124"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nicknameは8文字以内で入力してください")
+      end
+      it "passwordが英数字混合でなければ登録できない" do
+        @user.password = "aaaaaa"
+        @user.password_confirmation = "aaaaaa"
+        expect(@user.errors.full_messages).to include("半角6文字以上で英字と数字の両方を含めて設定してください")
+      end
     end
-    it "passwordが6文字以上の英数字混合であれば登録できる" do
-      @user.password = "aaa000"
-      @user.password_confirmation = "aaa000"
-      expect(@user).to be_valid 
-    end
+
   end
 end
 
-end
 
 
-    # it "nicknameが空だと登録できない" do
-    #   user = User.new(nickname: "", email: "testtest@gmail.com", password: "00000000", password_confirmation: "00000000")
-    #   user.valid?
-    # end
-    # it "emailが空では登録できない" do
-    #   user = User.new(nickname: "@user.nickname", email: "", password: "00000000", password_confirmation: "00000000")
-    #   user.valid?
-    # end
 
