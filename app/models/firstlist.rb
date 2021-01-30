c74ec96ba870d7d50932b3dbcee1484447853326
ywrk <ywrk.wprg@gmail.com>
2021-01-30 12:25:15 +0900

    Update firstlist.rb

--- app/models/firstlist.rb
+++ app/models/firstlist.rb
@@ -5,12 +5,6 @@ class Firstlist < ApplicationRecord
     validates :title, presence: { message: "タイトルを入力してください"}
     validates :text1, presence: { message: "リスト１を入力してください"}
   end
-  
-  # validates :text2
-  # validates :text3
-  # validates :text4
-  # validates :text5
-  # validates :text6
 
   # def self.auto_select
   # end
